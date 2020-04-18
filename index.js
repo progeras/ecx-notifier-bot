814b585e163a92502798f550a3555664016ee387
Asan Shamratov <ashamratov@eccentex.com>
2020-04-18 20:47:31 +0300

    Init

---
+++ index.js
@@ -0,0 +1,45 @@
+require('dotenv').config();
+const Telegraf = require('telegraf');
+const { telegram } = require('./telegram');
+const session = require('telegraf/session');
+const rp = require('request-promise');
+const express = require('express');
+const app = express();
+
+const bot = new Telegraf(process.env.TELEGRAM_TOKEN);
+bot.use(session());
+
+bot.on('message', (ctx) => ctx.reply(`ChatId: ${ctx.chat.id}`))
+
+process.env.NODE_ENV === 'development' ? startDevMode(bot) : startProdMode(bot);
+
+function startDevMode(bot) {
+    rp(`https://api.telegram.org/bot${process.env.TELEGRAM_TOKEN}/deleteWebhook`).then(
+        () => bot.startPolling(),
+        () => bot.startPolling()
+    );
+}
+
+function startProdMode(bot) {
+    const API_TOKEN = process.env.TELEGRAM_TOKEN || '';
+    const PORT = process.env.PORT || 3000;
+    const URL = process.env.URL;
+
+    bot.telegram.setWebhook(`${URL}/bot${API_TOKEN}`);
+    bot.startWebhook(`/bot${API_TOKEN}`, null, PORT)
+}
+
+app.get('/notify', function (req, res) {
+    telegram.sendMessage(req.query.chatid, req.query.message).then(() => {
+        res.send(`Message sent to ${req.query.chatid}`);
+    }).catch((err) => {
+        res.send(err);
+    });
+    //res.send(`Message sent to ${req.query.chatid}`);
+});
+
+app.listen(3001, function () {
+    console.log('Listening on port 3000!');
+});
+
+bot.launch();
